3d87d3cc2da5c0a2d36580dc19eaa0e397b7eab5
Emiliano Iacopini <iacoviola.tb@gmail.com>
2022-12-31 15:25:39 +0100

    initial commit

---
+++ trakt_backup.py
@@ -0,0 +1,137 @@
+import datetime
+import os
+import sys
+import subprocess
+import trakt_request
+import pandas
+import json
+import xml.etree.ElementTree as ET
+
+accepted_file_types = ['json', 'csv', 'xml']
+
+def launch_file(filepath):
+    if sys.platform.startswith('darwin'):
+        subprocess.call(('open', filepath))
+    elif os.name == 'nt':
+        os.startfile(filepath)
+    elif os.name == 'posix':
+        subprocess.call(('xdg-open', filepath))
+
+def flatten_json(item):
+    record = dict()
+    for key, value in item.items():
+        if not isinstance(value, dict):
+            record[key] = value
+        else:
+            record_nest = flatten_json(value)
+            for key_nest, value_nest in record_nest.items():
+                record[f'{key}_{key_nest}'] = value_nest
+    return record
+
+def get_dataframe_csv(path, file):
+    if file.startswith('stats'):
+        return None
+
+    with open(os.path.join(path, file), 'r') as f:
+        json_data = json.load(f)
+        list_new_data = list()
+
+        print(f'{len(json_data)} records in file: {file}')
+        if not len(json_data) == 0:
+            for item in json_data:
+                list_new_data.append(flatten_json(item))
+            return pandas.DataFrame(list_new_data)
+        else:
+            print(f'No data in file: {file}')
+            return None
+
+def nested_dict_to_xml(root, d):
+    for key, value in d.items():
+        if not isinstance(value, dict):
+            ET.SubElement(root, key).text = str(value)
+        else:
+            nested_dict_to_xml(ET.SubElement(root, key), value)
+
+def get_tree_xml(path, file):
+    if file.startswith('stats'):
+            return None
+
+    with open(os.path.join(path, file), 'r') as f:
+        json_data = json.load(f)
+
+    root = ET.Element('root')
+    if not len(json_data) == 0:
+        for item in json_data:
+            record = ET.SubElement(root, 'record')
+            for key, value in item.items():
+                if not isinstance(value, dict):
+                    ET.SubElement(record, key).text = str(value)
+                else:
+                    nested_dict_to_xml(ET.SubElement(record, key), value)
+        return ET.ElementTree(root)
+    else:
+        print(f'No data in file: {file}')
+        return None
+
+def convert(file_type, path):
+    for file in os.listdir(path):
+        if file_type == 'csv':
+            dataframe = get_dataframe_csv(path, file)
+            if dataframe is not None:
+                dataframe.to_csv(os.path.join(path, file.replace('.json', '.csv')), index=False)
+            if not file.startswith('stats'):
+                os.remove(os.path.join(path, file))
+        elif file_type == 'xml':
+            tree = get_tree_xml(path, file)
+            if tree is not None:
+                tree.write(os.path.join(path, file.replace('.json', '.xml')))
+                dataframe = pandas.read_xml(os.path.join(path, file.replace('.json', '.xml')))
+                dataframe.to_xml(os.path.join(path, file.replace('.json', '.xml')), index=False)
+            if not file.startswith('stats'):
+                os.remove(os.path.join(path, file))
+
+folder = input(f"Save files here (shell current working directory) ? [Y/n]\n(files will otherwise be saved in {os.path.expanduser('~')}): ")
+
+if folder.upper() == 'Y':
+    root = os.getcwd()
+    root = os.path.join(root, 'trakt_backup')
+else:
+    root = os.path.expanduser('~')
+    root = os.path.join(root, 'trakt_backup')
+
+print(f'Files will be saved in: {root}')
+
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    username = input('Enter your Trakt username: ')
+
+if len(sys.argv) > 2 and sys.argv[2] in accepted_file_types:
+    file_type = sys.argv[2]
+else:
+    print('Unsupported or no file type specified, defaulting to json')
+    file_type = 'json'
+
+if not os.path.exists(root):
+    os.makedirs(root)
+
+timestamp = os.path.getmtime(root)
+backup_folder_name = os.path.join(root, str(datetime.datetime.now().replace(microsecond=0)).replace(':', '.'))
+os.makedirs(backup_folder_name)
+
+try:
+    trakt_request.create_data_files(username, backup_folder_name)
+except Exception as e:
+    print(e)
+    os.rmdir(backup_folder_name)
+    sys.exit()
+
+if file_type != 'json':
+    convert(file_type, backup_folder_name)
+
+new_backup_folder_name = f'{backup_folder_name} {file_type}'
+os.rename(backup_folder_name, new_backup_folder_name)
+
+launch_file(new_backup_folder_name)
+print('Done.')
---
+++ trakt_request.py
@@ -0,0 +1,104 @@
+import requests
+import json
+import os
+
+def create_data_files(usr, root):
+    global username
+    username = usr
+    
+    global url
+    url = f'{url}/{username}'
+
+    global root_path
+    root_path = root
+
+    get_watched_movies()
+    get_watched_episodes()
+    get_watched_shows()
+    get_movies_ratings()
+    get_episodes_ratings()
+    get_shows_ratings()
+    get_seasons_ratings()
+    get_movies_history()
+    get_episodes_ratings()
+    get_movies_watchlist()
+    get_episodes_history()
+    get_shows_watchlist()
+    get_movies_collection()
+    get_episodes_collection()
+    get_shows_collection()
+    get_user_stats()
+
+def get(action, type):
+    print(f'Obtaining: {url}/{action}/{type}')
+    response = requests.get(f'{url}/{action}/{type}?limit=100000' , headers=headers)
+
+    if response.status_code == 404:
+        raise Exception(f'Error: user {username} not found')
+    elif response.status_code != 200:
+        print(f'An error as occured with code: {response.status_code} for operation {action}/{type}')
+        return
+    
+    if response.json() == []:
+        print(f'No {type} found in {action}')
+        return
+
+    file_watched = open(os.path.join(root_path, f'{action}_{type}.json'), 'w')
+    file_watched.write(json.dumps(response.json(), separators=(',', ':'), indent=4))
+    file_watched.close()
+    print(f'Obtained: {url}/{action}/{type}')
+
+def get_watched_movies():
+    get('watched', 'movies')
+
+def get_watched_episodes():
+    get('watched', 'episodes')
+
+def get_watched_shows():
+    get('watched', 'shows')
+
+def get_movies_ratings():
+    get('ratings', 'movies')
+
+def get_episodes_ratings():
+    get('ratings', 'episodes')
+
+def get_shows_ratings():
+    get('ratings', 'shows')
+
+def get_seasons_ratings():
+    get('ratings', 'seasons')
+
+def get_movies_history():
+    get('history', 'movies')
+
+def get_episodes_history():
+    get('history', 'episodes')
+
+def get_movies_watchlist():
+    get('watchlist', 'movies')
+
+def get_shows_watchlist():
+    get('watchlist', 'shows')
+
+def get_movies_collection():
+    get('collection', 'movies')
+
+def get_episodes_collection():
+    get('collection', 'episodes')
+
+def get_shows_collection():
+    get('collection', 'shows')
+
+def get_user_stats():
+    get('stats', '')
+
+url = 'https://api.trakt.tv/users'
+root_path = ''
+username = ''
+
+headers = {
+    'Content-Type': 'application/json',
+    'trakt-api-version': '2',
+    'trakt-api-key': ''
+}
